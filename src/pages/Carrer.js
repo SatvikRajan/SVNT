4d6f1395113b6a00944fe2c825525841a84a2c92
Satvik Rajan <satvikrajan@gmail.com>
2024-03-07 18:57:14 +0530

    update

--- src/pages/Carrer.js
+++ src/pages/Carrer.js
@@ -10,6 +10,29 @@ export const CareersPage = () => {
   const handleApplyClick = () => {
     setShowForm(true);
   };
+
+  const svg = <svg style={{marginLeft: '50px', marginRight: '25px'}} width="30" height="23" viewBox="0 0 30 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M8.20516 0H0L8.20516 10.7812L0 23H8.20516L16.1539 10.7812L8.20516 0Z" fill="url(#paint0_linear_1590_3086)"/>
+  <path d="M13.3317 0H10.7676L18.4599 10.6786L10.7676 23H13.3317L21.024 10.6786L13.3317 0Z" fill="url(#paint1_linear_1590_3086)"/>
+  <path d="M23.8457 10.6786L16.1533 0H22.3072L29.9995 10.6786L22.3072 23H16.1533L23.8457 10.6786Z" fill="url(#paint2_linear_1590_3086)"/>
+  <defs>
+  <linearGradient id="paint0_linear_1590_3086" x1="4.21406" y1="11.0893" x2="16.0149" y2="11.1373" gradientUnits="userSpaceOnUse">
+  <stop offset="0.0661152" stop-color="#7F57E9"/>
+  <stop offset="0.510295" stop-color="#4E28D1"/>
+  <stop offset="1" stop-color="#1A1741"/>
+  </linearGradient>
+  <linearGradient id="paint1_linear_1590_3086" x1="13.4432" y1="11.0893" x2="20.9359" y2="11.1087" gradientUnits="userSpaceOnUse">
+  <stop offset="0.0661152" stop-color="#7F57E9"/>
+  <stop offset="0.510295" stop-color="#4E28D1"/>
+  <stop offset="1" stop-color="#1A1741"/>
+  </linearGradient>
+  <linearGradient id="paint2_linear_1590_3086" x1="19.7654" y1="11.0893" x2="29.8804" y2="11.1246" gradientUnits="userSpaceOnUse">
+  <stop offset="0.0661152" stop-color="#7F57E9"/>
+  <stop offset="0.510295" stop-color="#4E28D1"/>
+  <stop offset="1" stop-color="#1A1741"/>
+  </linearGradient>
+  </defs>
+  </svg>
   return (
     <div className="careers-page">
       <div className="career-start">
