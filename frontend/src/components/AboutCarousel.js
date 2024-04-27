1d86bcb3fb870f9f2c28302fd1ef64bfa5945108
Harshita <harshitadutta11@gmail.com>
2024-04-27 21:02:02 +0530

    minor changes

--- frontend/src/components/AboutCarousel.js
+++ frontend/src/components/AboutCarousel.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Slider from "react-slick";
 import aboutbg from '../images/AboutUs/about-bg.jpg'
 import aboutbg1 from '../images/AboutUs/about-bg1'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function AboutCarousel() {
   const settings = {
@@ -29,4 +31,4 @@ function AboutCarousel() {
   )
 }
 
-export default AboutCarousel
+export default AboutCarousel;
