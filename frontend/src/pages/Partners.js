ec7e1c447faecc7fa566288509e73e854ddb4bdd
Satvik Rajan <satvikrajan@gmail.com>
2024-08-01 19:41:43 +0530

    update

--- frontend/src/pages/Partners.js
+++ frontend/src/pages/Partners.js
@@ -245,11 +245,11 @@ export default function Partners() {
                 </div>
             </div>
             <div className='d-flex certification'>
-                <div data-aos='fade-left' className='cert1'>
+                <div data-aos='fade-right' className='cert1'>
                     <h1 className='certification-h'>Joint Recognition</h1>
                     <p className='certification-p'>Each solution is tailored to understand and address the unique needs of each client. Everything we do is shaped by a core set of values. Our mission as a company revolves around the complete satisfaction of our clients</p>
                 </div>
-                <div data-aos='fade-right' >
+                <div data-aos='fade-left' >
                     <img className='cert' src={certification}></img>
                 </div>
             </div>
